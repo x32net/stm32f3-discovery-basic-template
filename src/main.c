6bc2accfe9a48f275fffd97b4de7e07d48c04b72
Matt <matt@home.mblythe.net>
2013-01-19 22:46:50 -0700

    accelerometer seems to work

--- src/main.c
+++ src/main.c
@@ -1,13 +1,27 @@
 #include "stm32f30x.h"
 #include "stm32f3_discovery.h"
+#include "stm32f3_discovery_lsm303dlhc.h"
+
+#define ABS(x)         (x < 0) ? (-x) : x
+#define PI                         (float)     3.14159265f
+
+#define LSM_Acc_Sensitivity_2g     (float)     1.0f            /*!< accelerometer sensitivity with 2 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_4g     (float)     0.5f            /*!< accelerometer sensitivity with 4 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_8g     (float)     0.25f           /*!< accelerometer sensitivity with 8 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_16g    (float)     0.0834f         /*!< accelerometer sensitivity with 12 g full scale [LSB/mg] */
 
 /* Private variables ---------------------------------------------------------*/
   RCC_ClocksTypeDef RCC_Clocks;
 __IO uint32_t TimingDelay = 0;
+__IO uint8_t DataReady = 0;
+float AccBuffer[3] = {0.0f};
+float AbsAccBuffer[3] = {0.0f};
 
 /* Private function prototypes -----------------------------------------------*/
 void TimingDelay_Decrement(void);
 void Delay(__IO uint32_t nTime);
+void Demo_CompassConfig(void);
+void Demo_CompassReadAcc(float* pfData);
 
 /* Private functions ---------------------------------------------------------*/
 
@@ -19,6 +33,7 @@ void Delay(__IO uint32_t nTime);
 void SysTick_Handler(void)
 {
   TimingDelay_Decrement();
+  DataReady ++;
 }
 
 /**
@@ -41,35 +56,421 @@ int main(void)
   STM_EVAL_LEDInit(LED8);
   STM_EVAL_LEDInit(LED9);
   STM_EVAL_LEDInit(LED10);
+    
+  /* LEDs Off */
+  STM_EVAL_LEDOff(LED4);
+  STM_EVAL_LEDOff(LED3);
+  STM_EVAL_LEDOff(LED6);
+  STM_EVAL_LEDOff(LED7);
+  STM_EVAL_LEDOff(LED10);
+  STM_EVAL_LEDOff(LED8);
+  STM_EVAL_LEDOff(LED9);
+  STM_EVAL_LEDOff(LED5);
+  
+  /* Demo Compass & accelerometer */
+  Demo_CompassConfig();
+  
+  DataReady = 0x00;
    
   /* Infinite loop */
   while (1)
   {   
-    /* LEDs Off */
-    STM_EVAL_LEDOff(LED3);
-    STM_EVAL_LEDOff(LED6);
-    STM_EVAL_LEDOff(LED7);
-    STM_EVAL_LEDOff(LED4);
-    STM_EVAL_LEDOff(LED10);
-    STM_EVAL_LEDOff(LED8);
-    STM_EVAL_LEDOff(LED9);
-    STM_EVAL_LEDOff(LED5);
+        
+    /* Wait for data ready */
+    while(DataReady !=0x05)
+    {}
+    DataReady = 0x00;
     
-    Delay(50); /*500ms - half second*/
+    /* Read Compass data */
+//    Demo_CompassReadMag(MagBuffer);
+    Demo_CompassReadAcc(AccBuffer);
+      
+    for(int i=0;i<3;i++) {
+      AccBuffer[i] /= 100.0f;
+    }
+      /*
+    fNormAcc = sqrt((AccBuffer[0]*AccBuffer[0])+(AccBuffer[1]*AccBuffer[1])+(AccBuffer[2]*AccBuffer[2]));
+      
+    fSinRoll = -AccBuffer[1]/fNormAcc;
+    fCosRoll = sqrt(1.0-(fSinRoll * fSinRoll));
+    fSinPitch = AccBuffer[0]/fNormAcc;
+    fCosPitch = sqrt(1.0-(fSinPitch * fSinPitch));
+    if ( fSinRoll >0)
+    {
+      if (fCosRoll>0)
+      {
+        RollAng = acos(fCosRoll)*180/PI;
+      }
+      else
+      {
+        RollAng = acos(fCosRoll)*180/PI + 180;
+      }
+    }
+    else
+    {
+      if (fCosRoll>0)
+      {
+        RollAng = acos(fCosRoll)*180/PI + 360;
+      }
+      else
+      {
+        RollAng = acos(fCosRoll)*180/PI + 180;
+      }
+    }
     
-    /* LEDs Off */
-    STM_EVAL_LEDOn(LED3);
-    STM_EVAL_LEDOn(LED6);
-    STM_EVAL_LEDOn(LED7);
-    STM_EVAL_LEDOn(LED4);
-    STM_EVAL_LEDOn(LED10);
-    STM_EVAL_LEDOn(LED8);
-    STM_EVAL_LEDOn(LED9);
-    STM_EVAL_LEDOn(LED5);
+    if ( fSinPitch >0)
+    {
+      if (fCosPitch>0)
+      {
+           PitchAng = acos(fCosPitch)*180/PI;
+      }
+      else
+      {
+         PitchAng = acos(fCosPitch)*180/PI + 180;
+      }
+    }
+    else
+    {
+      if (fCosPitch>0)
+      {
+           PitchAng = acos(fCosPitch)*180/PI + 360;
+      }
+      else
+      {
+         PitchAng = acos(fCosPitch)*180/PI + 180;
+      }
+    }
     
-    Delay(50); /*500ms - half second*/
+    if (RollAng >=360)
+    {
+      RollAng = RollAng - 360;
+    }
+     
+    if (PitchAng >=360)
+    {
+      PitchAng = PitchAng - 360;
+    }*/
+    
+    for(int i=0; i<3; i++){
+      AbsAccBuffer[i] = ABS(AccBuffer[i]);
+    }
+    if (  AbsAccBuffer[0] > 8*AbsAccBuffer[1]
+       && AbsAccBuffer[0] > 8*AbsAccBuffer[2] ){
+      if(AccBuffer[0] > 0){
+        STM_EVAL_LEDOn(LED10);
+        
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+      }
+      else{
+        STM_EVAL_LEDOn(LED3);
+        
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED10);
+      }
+    }
+    else if (  AbsAccBuffer[1] > 8*AbsAccBuffer[0]
+            && AbsAccBuffer[1] > 8*AbsAccBuffer[2] ){
+      if(AccBuffer[1] > 0){
+        STM_EVAL_LEDOn(LED7);
+        
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED10);
+      }
+      else{
+        STM_EVAL_LEDOn(LED6);
+        
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED10);
+      }
+    }
+    else if (  AbsAccBuffer[2] > 8*AbsAccBuffer[1]
+            && AbsAccBuffer[2] > 8*AbsAccBuffer[0] ){
+      if(AccBuffer[2] > 0){
+        STM_EVAL_LEDOn(LED5);
+        STM_EVAL_LEDOn(LED8);
+        
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED10);
+      }
+      else{
+        STM_EVAL_LEDOn(LED4);
+        STM_EVAL_LEDOn(LED9);
+        
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED10);
+      }
+    }
+    else{
+      /* LEDs Off */
+      STM_EVAL_LEDOff(LED4);
+      STM_EVAL_LEDOff(LED3);
+      STM_EVAL_LEDOff(LED6);
+      STM_EVAL_LEDOff(LED7);
+      STM_EVAL_LEDOff(LED10);
+      STM_EVAL_LEDOff(LED8);
+      STM_EVAL_LEDOff(LED9);
+      STM_EVAL_LEDOff(LED5);
+    }
+     
+    //fTiltedX = MagBuffer[0]*fCosPitch+MagBuffer[2]*fSinPitch;
+    //fTiltedY = MagBuffer[0]*fSinRoll*fSinPitch+MagBuffer[1]*fCosRoll-MagBuffer[1]*fSinRoll*fCosPitch;
+      
+    //HeadingValue = (float) ((atan2f((float)fTiltedY,(float)fTiltedX))*180)/PI;
+ 
+    /*if (HeadingValue < 0)
+    {
+      HeadingValue = HeadingValue + 360;    
+    }*/
+    /*  
+    if ((RollAng <= 40.0f) && (PitchAng <= 40.0f))
+    {
+      if (((HeadingValue < 25.0f)&&(HeadingValue >= 0.0f))||((HeadingValue >=340.0f)&&(HeadingValue <= 360.0f)))
+      {
+        STM_EVAL_LEDOn(LED10);
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED5);
+      }
+      else  if ((HeadingValue <70.0f)&&(HeadingValue >= 25.0f))
+      {
+        STM_EVAL_LEDOn(LED9);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED7);
+      } 
+      else  if ((HeadingValue < 115.0f)&&(HeadingValue >= 70.0f))
+      {
+        STM_EVAL_LEDOn(LED7);
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED5);
+      }
+      else  if ((HeadingValue <160.0f)&&(HeadingValue >= 115.0f))
+      {
+        STM_EVAL_LEDOn(LED5);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED3);
+      } 
+      else  if ((HeadingValue <205.0f)&&(HeadingValue >= 160.0f))
+      {
+        STM_EVAL_LEDOn(LED3);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED7);
+      } 
+      else  if ((HeadingValue <250.0f)&&(HeadingValue >= 205.0f))
+      {
+        STM_EVAL_LEDOn(LED4);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED7);
+      } 
+      else  if ((HeadingValue < 295.0f)&&(HeadingValue >= 250.0f))
+      {
+        STM_EVAL_LEDOn(LED6);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED8);
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED5);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED7);
+      }        
+      else  if ((HeadingValue < 340.0f)&&(HeadingValue >= 295.0f))
+      {
+        STM_EVAL_LEDOn(LED8);
+        STM_EVAL_LEDOff(LED6);
+        STM_EVAL_LEDOff(LED10);
+        STM_EVAL_LEDOff(LED7);
+        STM_EVAL_LEDOff(LED9);
+        STM_EVAL_LEDOff(LED3);
+        STM_EVAL_LEDOff(LED4);
+        STM_EVAL_LEDOff(LED5);
+      }
+    }
+    else
+    {
+      /* Toggle All LEDs * /
+      STM_EVAL_LEDToggle(LED7);
+      STM_EVAL_LEDToggle(LED6);
+      STM_EVAL_LEDToggle(LED10);
+      STM_EVAL_LEDToggle(LED8);
+      STM_EVAL_LEDToggle(LED9);
+      STM_EVAL_LEDToggle(LED3);
+      STM_EVAL_LEDToggle(LED4);
+      STM_EVAL_LEDToggle(LED5);
+      /* Delay 50ms * /
+      Delay(5);
+    }*/
   }
 }
+
+/**
+  * @brief  Configure the Mems to compass application.
+  * @param  None
+  * @retval None
+  */
+void Demo_CompassConfig(void)
+{
+  LSM303DLHCMag_InitTypeDef LSM303DLHC_InitStructure;
+  LSM303DLHCAcc_InitTypeDef LSM303DLHCAcc_InitStructure;
+  LSM303DLHCAcc_FilterConfigTypeDef LSM303DLHCFilter_InitStructure;
+  
+  /* Configure MEMS magnetometer main parameters: temp, working mode, full Scale and Data rate */
+  LSM303DLHC_InitStructure.Temperature_Sensor = LSM303DLHC_TEMPSENSOR_DISABLE;
+  LSM303DLHC_InitStructure.MagOutput_DataRate =LSM303DLHC_ODR_30_HZ ;
+  LSM303DLHC_InitStructure.MagFull_Scale = LSM303DLHC_FS_8_1_GA;
+  LSM303DLHC_InitStructure.Working_Mode = LSM303DLHC_CONTINUOS_CONVERSION;
+  LSM303DLHC_MagInit(&LSM303DLHC_InitStructure);
+  
+   /* Fill the accelerometer structure */
+  LSM303DLHCAcc_InitStructure.Power_Mode = LSM303DLHC_NORMAL_MODE;
+  LSM303DLHCAcc_InitStructure.AccOutput_DataRate = LSM303DLHC_ODR_50_HZ;
+  LSM303DLHCAcc_InitStructure.Axes_Enable= LSM303DLHC_AXES_ENABLE;
+  LSM303DLHCAcc_InitStructure.AccFull_Scale = LSM303DLHC_FULLSCALE_2G;
+  LSM303DLHCAcc_InitStructure.BlockData_Update = LSM303DLHC_BlockUpdate_Continous;
+  LSM303DLHCAcc_InitStructure.Endianness=LSM303DLHC_BLE_LSB;
+  LSM303DLHCAcc_InitStructure.High_Resolution=LSM303DLHC_HR_ENABLE;
+  /* Configure the accelerometer main parameters */
+  LSM303DLHC_AccInit(&LSM303DLHCAcc_InitStructure);
+  
+  /* Fill the accelerometer LPF structure */
+  LSM303DLHCFilter_InitStructure.HighPassFilter_Mode_Selection =LSM303DLHC_HPM_NORMAL_MODE;
+  LSM303DLHCFilter_InitStructure.HighPassFilter_CutOff_Frequency = LSM303DLHC_HPFCF_16;
+  LSM303DLHCFilter_InitStructure.HighPassFilter_AOI1 = LSM303DLHC_HPF_AOI1_DISABLE;
+  LSM303DLHCFilter_InitStructure.HighPassFilter_AOI2 = LSM303DLHC_HPF_AOI2_DISABLE;
+
+  /* Configure the accelerometer LPF main parameters */
+  LSM303DLHC_AccFilterConfig(&LSM303DLHCFilter_InitStructure);
+}
+
+/**
+* @brief Read LSM303DLHC output register, and calculate the acceleration ACC=(1/SENSITIVITY)* (out_h*256+out_l)/16 (12 bit rappresentation)
+* @param pnData: pointer to float buffer where to store data
+* @retval None
+*/
+void Demo_CompassReadAcc(float* pfData)
+{
+  int16_t pnRawData[3];
+  uint8_t ctrlx[2];
+  uint8_t buffer[6], cDivider;
+  uint8_t i = 0;
+  float LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_2g;
+  
+  /* Read the register content */
+  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_CTRL_REG4_A, ctrlx,2);
+  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_OUT_X_L_A, buffer, 6);
+   
+  if(ctrlx[1]&0x40)
+    cDivider=64;
+  else
+    cDivider=16;
+
+  /* check in the control register4 the data alignment*/
+  if(!(ctrlx[0] & 0x40) || (ctrlx[1] & 0x40)) /* Little Endian Mode or FIFO mode */
+  {
+    for(i=0; i<3; i++)
+    {
+      pnRawData[i]=((int16_t)((uint16_t)buffer[2*i+1] << 8) + buffer[2*i])/cDivider;
+    }
+  }
+  else /* Big Endian Mode */
+  {
+    for(i=0; i<3; i++)
+      pnRawData[i]=((int16_t)((uint16_t)buffer[2*i] << 8) + buffer[2*i+1])/cDivider;
+  }
+  /* Read the register content */
+  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_CTRL_REG4_A, ctrlx,2);
+
+
+  if(ctrlx[1]&0x40)
+  {
+    /* FIFO mode */
+    LSM_Acc_Sensitivity = 0.25;
+  }
+  else
+  {
+    /* normal mode */
+    /* switch the sensitivity value set in the CRTL4*/
+    switch(ctrlx[0] & 0x30)
+    {
+    case LSM303DLHC_FULLSCALE_2G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_2g;
+      break;
+    case LSM303DLHC_FULLSCALE_4G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_4g;
+      break;
+    case LSM303DLHC_FULLSCALE_8G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_8g;
+      break;
+    case LSM303DLHC_FULLSCALE_16G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_16g;
+      break;
+    }
+  }
+
+  /* Obtain the mg value for the three axis */
+  for(i=0; i<3; i++)
+  {
+    pfData[i]=(float)pnRawData[i]/LSM_Acc_Sensitivity;
+  }
+
+}
+
 /**
   * @brief  Inserts a delay time.
   * @param  nTime: specifies the delay time length, in 10 ms.
@@ -93,6 +494,16 @@ void TimingDelay_Decrement(void)
   { 
     TimingDelay--;
   }
+}
+
+/**
+  * @brief  Basic management of the timeout situation.
+  * @param  None.
+  * @retval None.
+  */
+uint32_t LSM303DLHC_TIMEOUT_UserCallback(void)
+{
+  return 0;
 }
 
 #ifdef  USE_FULL_ASSERT
