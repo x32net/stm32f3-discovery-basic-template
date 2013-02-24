1f29ecc927bc10f9d74f783d30e958cd21147e98
Matthew Blythe <mblythester+git@gmail.com>
2013-02-24 01:32:57 -0700

    the beep should be louder now, but I'm not sure if there's much difference.

--- src/beep.c
+++ src/beep.c
@@ -13,9 +13,13 @@ __IO uint8_t BeepOn = 0;
 
 // PA10 - TIM2_CH4 (AF10)
 
+// PB4 - TIM8_CH2N (AF4)
+// PB8 - TIM8_CH2  (AF10)
+
 void beep_init() {
   // Enable the GPIO Clocks
   RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOA, ENABLE);
+  RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOB, ENABLE);
 
   // configure pins
   GPIO_InitTypeDef GPIO_InitStructure;
@@ -27,8 +31,14 @@ void beep_init() {
   GPIO_Init(GPIOA, &GPIO_InitStructure);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_10);
 
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_8;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_4);
+  GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_10);
+
   // Enable Timer clocks
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM8, ENABLE);
 
   /* Time Base configuration */
 
@@ -49,25 +59,28 @@ void beep_init() {
   TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
 
   TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  TIM_TimeBaseInit(TIM8, &TIM_TimeBaseStructure);
 
   /* Channel 1, 2, 3 and 4 Configuration in PWM mode */
   TIM_OCInitTypeDef  TIM_OCInitStructure;
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
   TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_Low;
   TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
   TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
 
   TIM_OCInitStructure.TIM_Pulse = 1250;
-  TIM_OC4Init(TIM2, &TIM_OCInitStructure); //set up channel 4
+  TIM_OC4Init(TIM2, &TIM_OCInitStructure); //set up TIM2 channel 4
+  TIM_OC2Init(TIM8, &TIM_OCInitStructure); //set up TIM8 channel 2
 
-  /* TIM1 counter enable */
+  /* counter disable */
   TIM_Cmd(TIM2, DISABLE);
+  TIM_Cmd(TIM8, DISABLE);
 
-  /* TIM1 Main Output Disable */
-//  TIM_CtrlPWMOutputs(TIM4, DISABLE); //this doesn't work on TIM4
+  /* Main Output Disable */
+  TIM_CtrlPWMOutputs(TIM8, ENABLE); //this doesn't work on TIM4
   BeepOn = 0;
 }
 
@@ -76,18 +89,25 @@ void beep_on(uint8_t high){
   if(high == 1){
     TIM_SetAutoreload(TIM2,1250);
     TIM_SetCompare4(TIM2, 625);
+    TIM_SetAutoreload(TIM8,1250);
+    TIM_SetCompare2(TIM8, 625);
   }
   else{
     TIM_SetAutoreload(TIM2,5000);
     TIM_SetCompare4(TIM2, 2500);
+    TIM_SetAutoreload(TIM8,5000);
+    TIM_SetCompare2(TIM8, 2500);
   }
   TIM_Cmd(TIM2, ENABLE);
+  TIM_Cmd(TIM8, ENABLE);
+  TIM_CtrlPWMOutputs(TIM8, ENABLE);
   BeepOn = 25;
 }
 
 void beep_tick(){
   if(BeepOn == 1){
     TIM_Cmd(TIM2, DISABLE);
+    TIM_Cmd(TIM8, DISABLE);
     BeepOn = 0;
   }
   else if(BeepOn != 0){
